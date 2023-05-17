a1cb5c462e863f345407d1d32046790321932a9e
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-05-17 16:47:40 +0900

    スクレイピングコードを関数化+余計な空白等削除

--- scraping.py
+++ scraping.py
@@ -1,23 +1,32 @@
 # %%
 import requests
+import re
 from bs4 import BeautifulSoup
-from typing import List
-url = "https://qiita.com/poorko/items/9140c75415d748633a10"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-for script in soup(["script", "style","header"]):
-    script.decompose()
-# %%
+
 #再帰的にテキスト部分を抜き取る(改行調整のため親ノードでget_textを使用しない)
-def get_text(node):
+def get_node_text(node):
     text = ""
     children = node.findChildren(recursive=False)
     if (node.name == "body" or node.name == "div" or node.name == "p") and len(children) > 0:
-        text = "".join([get_text(child) for child in children]) + "\n"
+        text = "".join([get_node_text(child) for child in children]) + "\n"
+    elif node.name == "br":
+        text = "\n"
     else:
         text = node.get_text().strip()
     return text
-lines = get_text(soup.find("body")).split("\n")
-#lines = [line for line in lines if line != ""] #余計な改行を消すけど可読性下がる
-print("\n".join(lines))
+
+def get_text(url:str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for script in soup(["script", "style","header","footer","img","nav"]):
+        script.decompose()
+    #lines = [line for line in lines if line != ""] #余計な改行を消すけど可読性下がる
+    text = get_node_text(soup.find("body"))
+    text = re.sub("\n+", "\n", text)
+    text = re.sub('[ 　]+', ' ', text)
+    return text
+# %%
+if __name__ == "__main__":
+    print(get_text("https://prtimes.jp/main/html/rd/p/000000027.000100410.html"))
+
 # %%
